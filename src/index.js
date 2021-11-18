3d03652aaa4c1d2d6daa2e0eacd2d962c3c066cc
VladimirBeria <volodymyr.dechtiaruk@gmail.com>
2021-11-18 04:00:52 +0200

    final

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ import './style/reset.scss';
 import './style/global.scss';
 import './style/header.scss';
 import './style/content.scss';
+import './style/side-nav.scss';
+
+import './js/view.js';
+
+if (typeof require !== 'undefined') {
+  var customSelect = require("custom-select").default;
+  require("custom-select/build/custom-select.css");
+}
+
+const mySelects = customSelect("#langSelector");
+
+console.log(mySelects);
